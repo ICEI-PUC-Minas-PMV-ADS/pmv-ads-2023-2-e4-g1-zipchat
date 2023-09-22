96f6615793ed858776286dc43edd362642042a3d
Fellipe Pereira <fellipe.pereira@supermix.com.br>
2023-09-22 20:17:59 -0300

    criando estrutura do back

---
+++ src/back-end/Domain/Model/Root.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Model
+{
+    public class Root
+    {
+        public string model { get; set; }
+        public string prompt { get; set; }
+        public int max_tokens { get; set; }
+        public int temperature { get; set; }
+    }
+}
---
+++ src/back-end/back-zipchat/Controllers/ChatZip.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace back_zipchat.Controllers
+{
+    [ApiController]
+    [Route("zip-chat")]
+    public class ChatZip : ControllerBase
+    {
+        private readonly HttpClient _httpClient;
+
+        public ChatZip(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> PostSintomas(string sintomas,
+            [FromServices] IConfiguration configuration)
+        {
+
+            return Ok();
+        }
+    }
+}
