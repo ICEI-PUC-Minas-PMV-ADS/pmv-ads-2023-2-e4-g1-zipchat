5cad92b0fdc9386aa49845f6a0cb0b8986cc71be
Vinicius Monteiro Fortino <vinicius.fortino@gmail.com>
2023-10-01 19:49:59 -0300

    swagger atualizado

--- src/back-end/back-zipchat/Program.cs
+++ src/back-end/back-zipchat/Program.cs
@@ -19,7 +19,38 @@ DotNetEnv.Env.Load();
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "ZipChat", Version = "v1" });
+
+    c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+    {
+        Description = "JWT Authorization header using the Bearer scheme. Exemplo: \"Bearer {token}\"",
+        Name = "Authorization",
+        In = ParameterLocation.Header,
+        Type = SecuritySchemeType.ApiKey,
+        Scheme = "Bearer"
+    });
+
+    c.AddSecurityRequirement(new OpenApiSecurityRequirement
+        {
+            {
+                new OpenApiSecurityScheme
+                {
+                Reference = new OpenApiReference
+                 {
+                    Type = ReferenceType.SecurityScheme,
+                    Id = "Bearer"
+                    },
+                      Scheme = "oauth2",
+                      Name = "Bearer",
+                      In = ParameterLocation.Header,
+
+                },
+                    new string[] {}
+                }
+        });
+});
 
 //Interfaces
 builder.Services.AddScoped<IAServiceInterface, IAService>();
