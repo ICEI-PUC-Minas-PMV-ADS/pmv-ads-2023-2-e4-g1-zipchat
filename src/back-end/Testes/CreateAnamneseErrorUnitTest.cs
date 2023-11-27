5ba00c6200083d088204a6ffb98563bce8085010
Guilherme C Gomes <58198111+GuilhermeCorrea21@users.noreply.github.com>
2023-11-26 21:10:00 -0300

    Add files via upload

---
+++ src/back-end/Testes/CreateAnamneseErrorUnitTest.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Net.Http;
+using back_zipchat.Models;
+using back_zipchat.Services;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Xunit;
+
+namespace back_zipchat.Tests
+{
+    public class CreateAnamneseErrorUnitTest
+    {
+
+        private readonly MongoDBService _mongoDBService;
+
+        public CreateAnamneseErrorUnitTest(MongoDBService mongoDBService)
+        {
+            _mongoDBService = mongoDBService;
+        }
+
+        // Exceção personalizada para representar a ausência de um campo obrigatório
+        public class MissingFieldException : Exception
+        {
+            public MissingFieldException(string fieldName)
+                : base("Ausência de um campo obrigatório")
+            {
+            }
+        }
+
+
+
+        [Fact]
+        public async Task CreateAnamnese_MissingField_ThrowsSpecificExceptionAsync()
+        {
+            // Arrange
+            AnamneseModel anamnese = new AnamneseModel
+            {
+                usuario = "Usuário Válido",
+                resultadoIA = "procurar medico"
+                // Campo sintomas ausente
+            };
+
+            // Act e Assert
+            await Assert.ThrowsAsync<MissingFieldException>(() => _mongoDBService.CreateAsync(anamnese));
+        }
+
+    }
+}
+
