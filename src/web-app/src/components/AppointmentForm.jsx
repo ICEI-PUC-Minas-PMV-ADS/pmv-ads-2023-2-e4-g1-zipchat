5394aad114c151330776dc0867ba264484a9702f
Lucas <lucolborges@hotmail.com>
2023-12-03 09:43:44 -0300

    feat: colsulta-cadastro-web

---
+++ src/web-app/src/components/AppointmentForm.jsx
@@ -0,0 +1,106 @@
+import React, { useState } from "react";
+import { useThemeProvider } from "../zustang/ThemeProvider";
+import IconClose from "./icons/IconClose";
+import Calendar from "react-calendar";
+
+import IconCalendar from "./icons/IconCalendar";
+
+function AppointmentForm({ onClose }) {
+  const [showCalendar, setShowCalendar] = useState(false)
+  const [errorMessage, setErrorMessage] = useState("");
+  const [form, setForm] = useState({
+    medico: "",
+    especialidade: "",
+    date: ""
+  })
+  const [date, setDate] = useState(new Date());
+
+  const { theme } = useThemeProvider();
+
+  const handleInput = (fieldName, value) => {
+    setForm({...form, [fieldName]: value})
+  }
+
+  const handleForm = () => {
+    const values = Object.values(form)
+    const emptyValue = values.some(value => value === "")
+    if(emptyValue) {
+      setErrorMessage("Por favor, preencha todos os campos.");
+      return
+    }
+
+    // PARTE API
+
+
+    //// 
+    
+    setErrorMessage("");
+    showCalendar(false)
+  }
+
+  return (
+    <div className={`fixed left- inset-0 bg-black bg-opacity-25 backdrop-blur-sm flex justify-center items-center`}>
+    <div className={`w-[600px] h-[500px] bg-slate-200 rounded-2xl flex relative`}>
+      <div className="p-1 flex flex-col flex-1 relative">
+        <div onClick={() => onClose(false)} className="self-end cursor-pointer">
+          <IconClose width={26} height={26} />
+        </div>
+
+        <div className={`flex-1 mt-1 rounded-bl-2xl rounded-br-2xl  ${showCalendar ? "backdrop-blur-sm" : ""} ${theme === 'dark' ? 'bg-gpt-gray' : 'bg-white'}`}>
+          <h4 className="text-center text-2xl">Marcar consulta</h4>
+
+          <div className="h-3/6 justify-evenly flex flex-col relative">
+            <input
+              name="medico"
+              type="text"
+              placeholder="Médico"
+              className={`text-lg outline-none ml-5  rounded-sm p-3 pb-1 w-4/5 border-b-2 ${theme === 'dark' ? 'bg-gpt-gray' : 'bg-white'}`}
+              value={form.medico}
+              onChange={(e) => handleInput("medico", e.target.value)}
+            />
+
+            <input
+              name="especialidade"
+              type="text"
+              placeholder="Especialidade"
+              className={`text-lg outline-none ml-5  rounded-sm p-3 pb-1 w-4/5 border-b-2 ${theme === 'dark' ? 'bg-gpt-gray' : 'bg-white'}`}
+              value={form.especialidade}
+              onChange={(e) => handleInput("especialidade", e.target.value)}
+
+            />
+
+            <div className="flex ml-5 flex-row items-center">
+              <h4 className={`${theme === 'dark' ? 'white' : 'text-gray-500'}`}>Data: </h4>
+              <div onClick={() => setShowCalendar(true)} className="cursor-pointer ml-4">
+                <IconCalendar width={30} height={30} className={`${theme === 'dark' ? 'white' : 'text-gray-500'}`} />
+              </div>
+            </div>
+
+            {showCalendar && (
+              <div className={`absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-3/4 border bg-white border-1 p-2 flex items-center rounded-3xl flex-col backdrop-blur-sm`}>
+                <Calendar onChange={(date) => handleInput("date", date)} value={date} className="w-full flex flex-col items-center text-black" />
+                <h4
+                  onClick={() => setShowCalendar(false)}
+                  className="p-2 cursor-pointer px-5 text-black"
+                >
+                  Fechar
+                </h4>
+              </div>
+            )}
+          </div>
+          {errorMessage && (
+              <p className="text-red-500 text-center mt-2">{errorMessage}</p>
+            )}
+          <button onClick={handleForm} className={` text-lg transition-all text-white py-2 px-10 rounded-md mt-4 mx-auto block
+          
+          ${theme === 'dark' ? 'bg-gpt-lightgray hover:bg-gpt-lightgray/40' : 'bg-what-gree-darker hover:bg-what-grenn'}`}>
+            Enviar
+          </button>
+        </div>
+      </div>
+    </div>
+  </div>
+  );
+}
+
+export default AppointmentForm;
--- src/web-app/src/components/ChatMessage.jsx
+++ src/web-app/src/components/ChatMessage.jsx
@@ -4,20 +4,44 @@ import IconRobot from "./icons/IconRobot";
 import { useThemeProvider } from "../zustang/ThemeProvider";
 
 function ChatMessage({ item }) {
-  const {theme} = useThemeProvider();
+  const [showModal, setShowModal] = React.useState(false)
+  
+  const { theme } = useThemeProvider();
   return (
-    <div className={`py-5 ${item.author === "ai" && `${theme === 'dark' ? 'bg-gray-600/50' : 'bg-gray-600/10'}`}`}>
+    <div
+      className={`py-5 ${
+        item.author === "ai" &&
+        `${theme === "dark" ? "bg-gray-600/50" : "bg-gray-600/10"}`
+      }`}
+    >
       <div className="max-w-4xl m-auto flex">
         <div
           className={`text-white w-10 h-10 flex justify-center items-center mx-4
-          md:ml-0 rounded ${item.author === 'ai' ? 'bg-green-900' : 'bg-blue-900'} `}
+          md:ml-0 rounded ${
+            item.author === "ai" ? "bg-green-900" : "bg-blue-900"
+          } `}
         >
-          {item.author === "me" && (<IconUser width={24} height={24} />)}
-          {item.author === "ai" && (<IconRobot width={24} height={24} />)}
+          {item.author === "me" && <IconUser width={24} height={24} />}
+          {item.author === "ai" && <IconRobot width={24} height={24} />}
         </div>
 
-        <div className="flex-1 text-base whitespace-pre-wrap">{item.body}</div>
+        <div className="flex-1 text-base whitespace-pre-wrap">
+          {item.body}
+          {item.author === "ai" && (
+            <h4
+              onClick={() => setShowModal(true)}
+              className={`mt-3   w-fit py-2 px-5 cursor-pointer  rounded-3xl transition-all ${
+                theme === "dark"
+                  ? "text-link-color hover:bg-gray-600"
+                  : "text-blue-900 hover:bg-gray-300"
+              }`}
+            >
+              Marcar consulta ?
+            </h4>
+          )}
+        </div>
       </div>
+      {showModal && <AppointmentForm onClose={setShowModal} />}
     </div>
   );
 }
